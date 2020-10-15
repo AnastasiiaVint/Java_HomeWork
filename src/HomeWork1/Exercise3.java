bc1f0436d31d5e18d17d13f831f6be6ca2437479
AnastasiiaVint <a.vintrovich@gmail.com>
2020-10-15 23:11:38 +0300

    ex 3

--- src/HomeWork1/Exercise3.java
+++ src/HomeWork1/Exercise3.java
@@ -1,8 +1,21 @@
-// ex3 - Вывести заданное количество случайных чисел с переходом и без перехода на новую строку.
+package HomeWork1;// ex3 - Вывести заданное количество случайных чисел с переходом и без перехода на новую строку.
 
+
+import java.util.Random;
 
 public class Exercise3 {
     public static void main(String[] args) {
 
+        for(int i = 0; i < 10; i++){
+            Random random1 = new Random();
+            System.out.println(random1.nextInt());
+        }
+
+        for (int i = 0; i < 10; i++) {
+            Random random2 = new Random();
+            System.out.print(random2.nextInt(1000));
+
+        }
     }
 }
+
